dc220c057badb3a83d18090197813363b68d5980
Abhishek Mishra <ideamonk@gmail.com>
2009-10-01 20:33:18 +0530

    putting it on github

---
+++ defaults/preferences/spojranktracker.js
@@ -0,0 +1,4 @@
+// See http://kb.mozillazine.org/Localize_extension_descriptions
+pref("extensions.spojranktracker@abhishek.mishra.description", "chrome://spojranktracker/locale/spojranktracker.properties");
+pref("srt.username", "");
+pref("srt.recheck", "2");
